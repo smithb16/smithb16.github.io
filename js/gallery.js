5a9fc912ab79436e37d2f22069eb172a9cd71ba0
Ben Smith <benjamin.linden.smith@gmail.com>
2020-10-27 18:47:42 -0400

    Complete current image updating in gallery.

---
+++ js/gallery.js
@@ -0,0 +1,25 @@
+function activateGallery() {
+  let thumbnails = document.querySelectorAll("#gallery-thumbs>div>img");
+  let mainImage = document.querySelector("#gallery-photo img");
+  let galleryInfo = document.querySelector("#gallery-info");
+  let imageTitle = galleryInfo.querySelector(".title");
+  let imageInfo = galleryInfo.querySelector(".description");
+
+  thumbnails.forEach(function (thumbnail) {
+    thumbnail.addEventListener("click", function () {
+      // set clicked image as main image.
+      let newImageSrc = thumbnail.dataset.largeVersion;
+      mainImage.setAttribute("src", newImageSrc);
+      mainImage.setAttribute("alt", thumbnail.alt);
+
+      // change which image is current
+      let currentClass = "current";
+      document.querySelector("." + currentClass).classList.remove(currentClass);
+      thumbnail.parentNode.classList.add(currentClass);
+
+      // change image title and info
+      imageTitle.innerHTML = thumbnail.dataset.title;
+      imageInfo.innerHTML = thumbnail.dataset.description;
+    });
+  });
+}
